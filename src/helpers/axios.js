4a2fd53b71bf58a7691d59212cbb076584c586f0
Emmanuel Ezeani <monaoffice20@gmail.com>
2023-11-30 09:58:02 +0100

    deploy push for staging

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = "https://integrations.getravenbank.com/v1";
-// const API = "https://baas.getraventest.com";
+// const API = "https://integrations.getravenbank.com/v1";
+const API = "https://baas.getraventest.com";
 // const API = 'https://adee-156-0-250-54.ngrok-free.app'
 
 /** base url to make request to the BE end point */
